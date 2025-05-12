e3e5fbeb9f2799757a1c4ca5b485ae5e1a2f8d29
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2025-05-12 16:04:09 -0400

    Uploading the Presentation Slide

--- main.py
+++ main.py
@@ -2,26 +2,47 @@ from algorithms.search_algorithms import astar, bfs, dfs, ucs
 from heuristics.heuristics import manhattan_distance, hamming_distance
 from visualization.visualizer import PuzzleVisualizer
 
+def select_start_state():
+    start_states = [
+        [[0, 2, 1],
+         [6, 3, 4],
+         [7, 5, 8]],
+        
+        [[2, 8, 3],
+         [1, 6, 4],
+         [7, 0, 5]]
+
+    ]
+
+    print("Choose a start state:")
+    for i, state in enumerate(start_states):
+        print(f"\n{i+1}.")
+        for row in state:
+            print(row)
+
+    choice = int(input("\nEnter the number of the start state: ")) - 1
+    if 0 <= choice < len(start_states):
+        return start_states[choice]
+    else:
+        print("Invalid choice. Defaulting to option 1.")
+        return start_states[0]
+
 def select_algorithm():
     print("\nChoose a search algorithm:")
     print("1. A* Search")
     print("2. Breadth-First Search (BFS)")
     print("3. Depth-First Search (DFS)")
     print("4. Uniform Cost Search (UCS)")
-    choice = input("Enter the number of the algorithm: ")
-    return choice.strip()
+    return input("Enter the number of the algorithm: ").strip()
 
 def select_heuristic():
     print("\nChoose a heuristic for A*:")
     print("1. Manhattan Distance")
     print("2. Hamming Distance")
-    choice = input("Enter the number of the heuristic: ")
-    return choice.strip()
+    return input("Enter the number of the heuristic: ").strip()
 
 if __name__ == "__main__":
-    start = [[0, 2, 1],
-             [6, 3, 4],
-             [7, 5, 8]]
+    start = select_start_state()
     goal = [[1, 2, 3],
             [8, 0, 4],
             [7, 6, 5]]
